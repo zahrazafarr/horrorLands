45d608b6ae3798d71c27b498b0b2af1af735cc6f
Zahra A <zahraa@Zahras-Air.home>
2022-11-23 11:48:01 -0500

    cards not showing in straight line/work on that later/now adding data to other pages

--- models/artSeed.js
+++ models/artSeed.js
@@ -4,5 +4,59 @@ module.exports = [
         img: "https://i0.wp.com/bloody-disgusting.com/wp-content/uploads/2020/02/Screen-Shot-2020-02-19-at-8.53.59-AM.png?resize=1024%2C564&ssl=1",
         link: "https://www.hollywoodreporter.com/movies/movie-news/tobin-bell-returning-saw-franchise-1235247393/",
         description: "Original Jigsaw killer John Kramer to return in next sequel set for an Oct. 27, 2023 release."
+    },
+    {
+        title: "Killer Kids",
+        img: "https://i0.wp.com/bloody-disgusting.com/wp-content/uploads/2022/10/daeg.png?w=1480&ssl=1",
+        link: "https://bloody-disgusting.com/editorials/3738433/killer-kids-celebrating-six-of-the-best-evil-child-performances-in-horror/",
+        description: "Six of the Best Evil Child Performances in Horror"
+    },
+    {
+        title: "'The Exorcist' Reboot",
+        img: "https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/10/the-exorcist0.jpg?q=50&fit=contain&w=943&h=&dpr=1.5",
+        link: "https://bloody-disgusting.com/movie/3737045/the-exorcist-david-gordon-greens-reboot-begins-filming-soon/",
+        description: "David Gordon Green Begins Filming Soon"
+    },
+    {
+        title: "'Can Experiencing Horror Help Your Brain?'",
+        img: "https://assets.vogue.com/photos/633c867470b406a345b0d2d0/master/w_960,c_limit/MSDISTK_CO009.jpg",
+        link: "https://www.smithsonianmag.com/science-nature/can-experiencing-horror-help-your-brain-180980984/",
+        description: '"..lab studies, survey studies, and real-world empirical studies to understand this widespread but scientifically understudied psychological phenomenon."'
+    },
+    {
+        title: "Terrifier 2 Now ON VHS",
+        img: "https://media-prod.fangoria.com/images/Terrifier2.width-800.jpg",
+        link: "https://www.fangoria.com/original/terrifer-2-is-getting-a-vhs-release-because-why-not/",
+        description: "If there's any movie that would be perfect for a VHS release, it's definitely this one."
+    },
+    {
+        title: "All About the Final Girl Trope",
+        img: "https://images.squarespace-cdn.com/content/v1/5578d503e4b0bc473a0165e5/1486412196342-QPXNVUCGR4HL92FJ18U2/image-asset.jpeg?format=750w",
+        link: "https://nofilmschool.com/final-girl-trope",
+        description: '"Calling all Sidney Precotts... we need you!"'
+    },
+    {
+        title: '"New Friday The 13th Movie Is Officially, And Finally, In The Works"',
+        img: "https://www.slashfilm.com/img/gallery/it-sounds-like-a-new-friday-the-13th-movie-is-officially-and-finally-in-the-works/intro-1662063923.webp",
+        link: "https://www.slashfilm.com/991048/it-sounds-like-a-new-friday-the-13th-movie-is-officially-and-finally-in-the-works/",
+        description: "NO IT'S NOT. AND THEY LIED."
+    },
+    {
+        title: '"Daytime Horror Returns with the TX Chain Saw Massacre Game"',
+        img: "https://cdn.vox-cdn.com/thumbor/h2jLDrePGGGgUQU6HDBF4crO5nI=/0x0:3840x2160/920x613/filters:focal(1613x773:2227x1387):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/71395923/ss_4c65bf6cb402eb29f9459ea8f36e6ab6590a5fa9.0.jpg",
+        link: "https://www.wired.com/story/texas-chain-saw-massacre-game-impressions/",
+        description: "Slash through the sunlight."
+    },
+    {
+        title: "'In Flames' First Look at the New Pakistani Horror Movie",
+        img: "https://i0.wp.com/bloody-disgusting.com/wp-content/uploads/2022/11/in-flames-2.png?w=1000&ssl=1",
+        link: "https://bloody-disgusting.com/movie/3741466/in-flames-first-look-pakistani-horror-movie-will-haunt-the-festival-circuit-in-2023/",
+        description: '"XYZ Films will handle North American sales rights on film"'
+    },
+    {
+        title: "Bill Skarsgard and Lily-Rose Depp in Talks to Lead Robert Eggers' 'Nosferatu'",
+        img: "https://cdn.mos.cms.futurecdn.net/oWyYzbosaPovDhXG3VbAsG-970-80.jpg.webp",
+        link: "https://deadline.com/2022/09/bill-skarsgard-lily-rose-depp-robert-eggers-nosferatu-focus-1235131507/",
+        description: "YESSSSS"
     }
 ]
