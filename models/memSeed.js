3d3c3de4088e556c60f71dd8933a5f6aad708d19
Zahra A <zahraa@Zahras-Air.home>
2022-11-24 21:56:20 -0500

    articles page still messy/photos dont show on memorabilia show page/cleaned up code/movies page still empty/still happy w this tho

--- models/memSeed.js
+++ models/memSeed.js
@@ -3,7 +3,99 @@ module.exports = [
         img: "https://www.trickortreatstudios.com/media/catalog/product/cache/1da4909b8e3ea5eea17a9fb4c6e4a516/_/0/_0007_seedofchucky-glen-productiondoll.png",
         product: "SEED OF CHUCKY - GLEN DOLL",
         price: "$499.99",
-        linkToBuy: "https://www.trickortreatstudios.com/seed-of-chucky-glen-doll.html",
+        linkToBuy: "https://www.trickortreatstudios.com/",
         canBuy: false
+    },
+    {
+        img: "./img/tonguephone.png",
+        product: "A Nightmare On Elm Street Tongue Phone Signed By Robert Englund",
+        price: "$675",
+        linkToBuy: "https://www.ebay.com/itm/265364008918",
+        canBuy: true
+    },
+
+    {
+        img: "./img/robzomb.png",
+        product: "Demon Speeding - Rob Zombie Figurine",
+        price: "Price unknown.",
+        linkToBuy: "https://www.deviantart.com/bars0m/art/Rob-Zombie-Demon-speeding-statue-Barsom-273377553",
+        canBuy: false
+    },
+    {
+        img: "./img/chuckyskull.png",
+        product: "Chucky's Good Guy Skull",
+        price: "$79.99",
+        linkToBuy: "https://www.sideshow.com/collectibles/childs-play-2-chucky-skull-good-guys-skull-trick-or-treat-studios-908117",
+        canBuy: true,
+    },
+    {
+        img: "./img/vampy.png",
+        product: "Vampirella (Color Variant)",
+        price: "$175",
+        linkToBuy: "https://www.sideshow.com/collectibles/vampirella-vampirella-color-variant-dynamite-entertainment-908925",
+        canBuy: true,
+    },
+    {
+        img: "./img/teddyjason.png",
+        product: "Ultimate Jason Voorhees Figure - Freddy vs Jason",
+        price: "$37.99",
+        linkToBuy: "https://www.bigbadtoystore.com/Product/VariationDetails/91180",
+        canBuy: true,
+    },
+    {
+        img: "",
+        product: "",
+        price: "$",
+        linkToBuy: "",
+        canBuy: "",
+    },
+    {
+        img: "",
+        product: "",
+        price: "$",
+        linkToBuy: "",
+        canBuy: "",
+    },
+    {
+        img: "",
+        product: "",
+        price: "$",
+        linkToBuy: "",
+        canBuy: "",
+    },
+    {
+        img: "",
+        product: "",
+        price: "$",
+        linkToBuy: "",
+        canBuy: "",
+    },
+    {
+        img: "",
+        product: "",
+        price: "$",
+        linkToBuy: "",
+        canBuy: "",
+    },
+    {
+        img: "",
+        product: "",
+        price: "$",
+        linkToBuy: "",
+        canBuy: "",
+    },
+    {
+        img: "",
+        product: "",
+        price: "$",
+        linkToBuy: "",
+        canBuy: "",
+    },
+    {
+        img: "",
+        product: "",
+        price: "$",
+        linkToBuy: "",
+        canBuy: "",
     }
 ]
